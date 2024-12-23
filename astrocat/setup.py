e835c77b3b098c883875a33a0e9461f12fa5d4b7
JChurchley91 <joseph.churchley@outlook.com>
2024-12-23 13:38:02 +0700

    structure added

---
+++ astrocat/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="astrocat",
+    packages=find_packages(exclude=["astrocat_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
